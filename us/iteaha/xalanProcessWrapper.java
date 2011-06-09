2278014f65ae69a06ae671c18efcc09f11eb22fd
hao <hao+github@iteaha.us>
2011-06-09 16:26:34 +0000

    first commit

---
+++ us/iteaha/xalanProcessWrapper.java
@@ -0,0 +1,8 @@
+package us.iteaha;
+import com.sun.org.apache.xalan.internal.xslt.Process;
+
+public class xalanProcessWrapper extends com.sun.org.apache.xalan.internal.xslt.Process {
+    public static void main( java.lang.String[] args ) {
+        Process._main(args);
+    }
+}
